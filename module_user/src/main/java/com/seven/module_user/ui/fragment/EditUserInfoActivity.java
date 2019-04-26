d63904c6caf890d1f6a1fa0576f21bbe4fafea6b
ouyang <344588727@qq.com>
2019-04-26 19:35:38 +0800

    4/26-19"35

--- module_user/src/main/java/com/seven/module_user/ui/fragment/EditUserInfoActivity.java
+++ module_user/src/main/java/com/seven/module_user/ui/fragment/EditUserInfoActivity.java
@@ -1,8 +1,19 @@
 package com.seven.module_user.ui.fragment;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.design.widget.BottomSheetDialog;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,20 +21,34 @@ import com.bigkoo.pickerview.builder.OptionsPickerBuilder;
 import com.bigkoo.pickerview.listener.OnOptionsSelectListener;
 import com.bigkoo.pickerview.view.OptionsPickerView;
 import com.seven.lib_common.base.activity.BaseTitleActivity;
+import com.seven.lib_common.utils.ToastUtils;
+import com.seven.lib_model.ApiManager;
+import com.seven.lib_model.BaseResult;
+import com.seven.lib_model.CommonObserver;
 import com.seven.module_user.R;
 import com.seven.module_user.R2;
+import com.tbruyelle.rxpermissions2.RxPermissions;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DefaultObserver;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 
 
 public class EditUserInfoActivity extends BaseTitleActivity {
 
-
+    private BottomSheetDialog mBottomSheetDialog;
+    private RxPermissions mRxPermissions;
+    private String TAG = EditUserInfoActivity.class.getName();
     @BindView(R2.id.user_photo)
     TextView userPhoto;
     @BindView(R2.id.user_nick_name)
@@ -68,7 +93,8 @@ public class EditUserInfoActivity extends BaseTitleActivity {
 
     @Override
     protected void initBundleData(Intent intent) {
-
+        mRxPermissions = new RxPermissions(this);
+        mRxPermissions.setLogging(true);
     }
 
     private void chooseSex() {
@@ -99,9 +125,172 @@ public class EditUserInfoActivity extends BaseTitleActivity {
     @OnClick({R2.id.user_photo, R2.id.choose_sex})
     public void onViewClicked(View view) {
         if (view.getId() == R.id.user_photo) {
-
+            onChangePic();
         } else if (view.getId() == R.id.choose_sex) {
             chooseSex();
         }
+    }
+
+    private void onChangePic() {
+        mBottomSheetDialog = new BottomSheetDialog(this);
+        View v = LayoutInflater.from(this).inflate(R.layout.dialog_choice_img, null, false);
+        v.findViewById(R.id.photo).setOnClickListener(this);
+        v.findViewById(R.id.camera).setOnClickListener(this);
+        v.findViewById(R.id.cancel).setOnClickListener(this);
+        mBottomSheetDialog.setContentView(v);
+        mBottomSheetDialog.show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        super.onClick(view);
+        if (view.getId() == R.id.cancel) {
+            mBottomSheetDialog.dismiss();
+        } else if (view.getId() == R.id.photo) {
+            selectGallery();
+            mBottomSheetDialog.dismiss();
+        } else if (view.getId() == R.id.camera) {
+            selectCapture();
+            mBottomSheetDialog.dismiss();
+        }
+    }
+
+    //选择相册
+    public void selectGallery() {
+        mRxPermissions.request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .subscribe(new Observer<Boolean>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Boolean aBoolean) {
+                        if (aBoolean) {
+                            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                            intent.setType("image/*");
+                            startActivityForResult(intent, 10086);
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+
+    public void selectCapture() {
+        mRxPermissions.request(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .subscribe(new Observer<Boolean>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Boolean aBoolean) {
+                        if (aBoolean) {
+                            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);//action is capture
+                            intent.putExtra("return-data", false);
+                           // intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                            intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+                            intent.putExtra("noFaceDetection", true);
+                            startActivityForResult(intent, 10010);
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            Uri uri;
+            String realUri;
+            File file;
+            switch (requestCode) {
+                case 10086:
+                    //选择照片
+                    uri = data.getData();
+                    realUri = uri2filePath(uri, mContext);
+                    file = new File(realUri);
+                    upLoad(file);
+                    break;
+                case 10010:
+                    //拍照
+                    uri = data.getData();
+                    realUri = uri2filePath(uri, mContext);
+                    file = new File(realUri);
+                    upLoad(file);
+                    break;
+
+            }
+        }
+    }
+
+    public String uri2filePath(Uri uri, Context context) {
+        String path = "";
+        if (uri.toString().startsWith("file")) {
+            return uri.getPath();
+        }
+        Cursor cursor = null;
+        try {
+            //android 4.4.4会获得另外一种图片uri地址:
+            if (Build.VERSION.SDK_INT >= 19 && DocumentsContract.isDocumentUri(context, uri)) {
+                String wholeID = DocumentsContract.getDocumentId(uri);
+                String id = wholeID.split(":")[1];
+                String[] column = {MediaStore.Images.Media.DATA};
+                String sel = MediaStore.Images.Media._ID + "=?";
+                cursor = context.getContentResolver().query(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, column, sel,
+                        new String[]{id}, null);
+                int columnIndex = cursor.getColumnIndex(column[0]);
+                if (cursor.moveToFirst()) {
+                    path = cursor.getString(columnIndex);
+                }
+            } else {
+                String[] projection = {MediaStore.MediaColumns.DATA};
+                cursor = context.getContentResolver().query(uri, projection, null, null, null);
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+                if (cursor.moveToFirst()) {
+                    path = cursor.getString(column_index);
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        } finally {
+            Log.i(TAG, "imageUrl:" + path);
+            cursor.close();
+        }
+        return path;
+    }
+
+    private void upLoad(File file) {
+        RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+        MultipartBody.Part body = MultipartBody.Part.create(requestBody);
+        ApiManager.upLoad(body)
+                .subscribe(new CommonObserver<BaseResult>() {
+                    @Override
+                    public void onNext(BaseResult baseResult) {
+                        ToastUtils.showToast(mContext, baseResult.getMessage());
+                    }
+                });
     }
 }
